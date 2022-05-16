df55a7d2577ddde6cdca4f6cd073be3a29d03f16
MasakatsuTagishi <m.t-10.22@docomo.ne.jp>
2022-05-16 09:12:42 +0900

    Initial Commit

---
+++ Practice-StrategyPattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice-StrategyPattern
+//
+//  Created by 田岸将勝 on 2022/05/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
